cfc8665903b66d7885b9702a31a6001a6a3bcbf8
bigwheel <k.bigwheel+eng@gmail.com>
2014-03-24 03:06:10 +0900

    mongodbに接続してjsonを取ってくるようにした

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,30 @@
 package controllers
 
 import play.api.mvc._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsObject, Json}
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.json.collection.JSONCollection
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-object Application extends Controller {
+object Application extends Controller with MongoController {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def notes(url: String) = Action {
-    Ok(Json.obj())
+  def notesCollection = db.collection[JSONCollection]("notes")
+
+  def notes(url: String) = Action.async {
+    val futureNotesList =
+      notesCollection.find(Json.obj("url" -> url)).cursor[JsObject].collect[List]()
+
+    val futureJson = futureNotesList.map { notesList =>
+      notesList.headOption match {
+        case Some(jsObject) => jsObject
+        case None => Json.obj()
+      }
+    }
+    futureJson.map(Ok(_))
   }
 
 }
