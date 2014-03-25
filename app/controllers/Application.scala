01e862370c8bbdf945ca8156ef6bbf9e4ca78c29
bigwheel <k.bigwheel+eng@gmail.com>
2014-03-26 07:13:32 +0900

    notesに大してRESTなREAD/CREATE APIを整備した
     * CREATE時にJSONのフォーマットをチェックしたい

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,13 @@
 package controllers
 
 import play.api.mvc._
-import play.api.libs.json.{JsObject, Json}
+import play.api.libs.json.{JsArray, JsObject, Json}
 import play.modules.reactivemongo.MongoController
 import play.modules.reactivemongo.json.collection.JSONCollection
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import reactivemongo.api.indexes.{IndexType, Index}
+import scala.concurrent.Future
+import reactivemongo.core.commands.LastError
 
 object Application extends Controller with MongoController {
 
@@ -22,17 +24,23 @@ object Application extends Controller with MongoController {
     }
   }
 
-  def notes(url: String) = Action.async {
+  def getNote(url: String) = Action.async {
     val futureNotesList =
       notesCollection.find(Json.obj("url" -> url)).cursor[JsObject].collect[List]()
 
-    val futureJson = futureNotesList.map { notesList =>
-      notesList.headOption match {
-        case Some(jsObject) => jsObject
-        case None => Json.obj()
-      }
-    }
+    val futureJson = futureNotesList.map (noteList => JsArray(noteList))
     futureJson.map(Ok(_))
+  }
+
+  def postNote(url: String) = Action.async(parse.json) { request =>
+    val futureLastError: Future[LastError] = notesCollection.save(request.body.as[JsObject] ++ Json.obj("url" -> url))
+
+    futureLastError.map { lastError =>
+      if (lastError.ok)
+        Ok
+      else
+        BadRequest
+    }
   }
 
 }
