35829a771ebf64edf1a763186d32e7a12d3c29a3
bigwheel <k.bigwheel+eng@gmail.com>
2014-04-13 02:17:10 +0900

    不要なコメントを削除した

--- test/controllers/ApplicationSpec.scala
+++ test/controllers/ApplicationSpec.scala
@@ -8,11 +8,6 @@ import play.api.test._
 import play.api.test.Helpers._
 import play.api.mvc.Controller
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
 
